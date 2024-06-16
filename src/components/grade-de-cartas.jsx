ecd8e8af1ac6e79e5c9595f245eca56fd2b2b7d1
Guilherme Ferraz Ronha <guilhermeferrazronha123@gmail.com>
2024-06-16 18:07:08 -0300

    Passar cartas para a grade no props

--- src/components/grade-de-cartas.jsx
+++ src/components/grade-de-cartas.jsx
@@ -1,18 +1,9 @@
-import jogo from '../classes/jogo.js'
 import '../styles/grade-de-cartas.css'
-import CartaFisica from './carta-fisica.jsx'
 
-export default function GradeDeCartas(){
-    jogo.iniciarJogo()
-
+export default function GradeDeCartas(props){
     return(
-        <div id="grade-de-cartas">
-            {jogo.cartas.map((carta, indice) => (
-                <CartaFisica
-                    key={indice}
-                    carta={carta}
-                ></CartaFisica>
-            ))}
+        <div id={props.id}>
+            {props.children}
         </div>
     )
 }
--- src/views/tela-jogo.jsx
+++ src/views/tela-jogo.jsx
@@ -1,11 +1,20 @@
-import GradeDeCartas from "../components/grade-de-cartas";
 import '../styles/tela-jogo.css'
+import jogo from '../classes/jogo.js'
+import GradeDeCartas from '../components/grade-de-cartas'
+import CartaFisica from '../components/carta-fisica.jsx'
 
 export default function TelaJogo(props){
+    jogo.iniciarJogo()
+    
     return(
         <div id="tela-jogo">
-            <GradeDeCartas>
-
+            <GradeDeCartas id="grade-de-cartas">
+                {jogo.cartas.map((carta, indice) => (
+                    <CartaFisica
+                        key={indice}
+                        carta={carta}
+                    ></CartaFisica>
+                ))}
             </GradeDeCartas>
         </div>
     )
